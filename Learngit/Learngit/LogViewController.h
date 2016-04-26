2301ea5c6aabe4516ea58ec36e4f678b640c7fbf
cwl <1178686275@qq.com>
2016-04-26 16:46:01 +0800

    add log

---
+++ Learngit/Learngit/LogViewController.h
@@ -0,0 +1,13 @@
+//
+//  LogViewController.h
+//  Learngit
+//
+//  Created by chengweilin on 16/4/26.
+//  Copyright © 2016年 chengweilin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LogViewController : UIViewController
+
+@end
