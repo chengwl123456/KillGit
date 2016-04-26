2972a043d6cd8f184cfcc1af18dff5bce21315f3
cwl <1178686275@qq.com>
2016-04-26 15:27:56 +0800

    add attribute

---
+++ Learngit/Learngit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Learngit
+//
+//  Created by chengweilin on 16/4/26.
+//  Copyright © 2016年 chengweilin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Learngit/Learngit/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Learngit
+//
+//  Created by chengweilin on 16/4/26.
+//  Copyright © 2016年 chengweilin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (nonatomic, strong)NSString *name;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    _name = @"小明";
+    NSLog(@"%@",_name);
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
